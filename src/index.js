428bc25654b1a1525729cc0c1e4abde0742375c4
16oh4 <gracia.v.bruno@gmail.com>
2020-03-28 08:50:29 -0500

    Fixed first commit

--- src/index.js
+++ src/index.js
@@ -21,13 +21,13 @@ const firebaseConfig = {
 };
 
 
-ReactDOM.createRoot(document.getElementById('root').render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <FirebaseAppProvider firebaseConfig={firebaseConfig}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </FirebaseAppProvider>
-));
+);
 
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
