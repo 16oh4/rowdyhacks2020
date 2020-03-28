a9ee2ff1dcae9fe5e612e1d2ef04600bdeed60b9
gabrielweinbrenner <angelgabe101@gmail.com>
2020-03-28 12:16:44 -0400

    Games matching

--- src/pages/Match.js
+++ src/pages/Match.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export default (props) => {
-    return (
-        <h1>Match</h1>
-    )
+export default class Match extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            games: []
+        }
+    }
+    getGames(){
+        fetch('https://api.rawg.io/api/games?dates=2017-01-01,2019-12-31&ordering=-added')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                var games = data.results;
+                var gamesData =[];
+                for(var i = 0; i < games.length; i++){
+                    gamesData.push({
+                        "name": games[i].name,
+                        "image": games[i].background_image,
+                        "genres": games[i].tags
+                    });
+                }
+                return data
+            })
+    }
+    componentDidMount(){
+        this.setState({games: this.getGames()})
+    }
+    render(){
+        console.log(this.state);
+        return (
+            <h1>Match</h1>
+        )
+    }
 }
