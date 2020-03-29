bbcfd7f67b4d4229876dc6c18acb256f33b923a4
16oh4 <gracia.v.bruno@gmail.com>
2020-03-29 01:55:53 -0500

    Final match page changes. started profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,49 @@
 import React from 'react';
 
+import {RowCreator, ColumnCreator, BlockCreator} from '../inc/PageCreator';
+
+const useStyles = makeStyles(({styles, palette}) => createStyles({
+    ...styles
+}));
+
 export default (props) => {
+
+    const classes = useStyles();
+
+    const profileBlock = (
+        <BlockCreator>
+            <form
+                name="profile_form"
+                autoComplete="off"
+                noValidate
+            >
+                <Grid
+                    container
+                >
+                    <Grid
+                        
+                    ></Grid>
+                </Grid>
+            </form>
+        </BlockCreator>
+    )
+
+    const matchesBlock = (
+
+    )
+
+    const mobileMarkup = (
+        <RowCreator>
+            <ColumnCreator
+                ratio={12}
+            >
+            
+            </ColumnCreator>
+        </RowCreator>
+    )
     return (
-        <h1>Profile</h1>
+        <>
+            {mobileMarkup}
+        </>
     )
 }
