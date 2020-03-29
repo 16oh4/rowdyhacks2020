3cdd4e0231e0171ea1787edf7379acfeacbdc16c
Bruno E. Gracia Villalobos <37783822+16oh4@users.noreply.github.com>
2020-03-29 13:24:52 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const app = require('express')();
 app.use(cors());
 
 // https://us-central1-team1604-e68a9.cloudfunctions.net/api
-// http://localhost:5001/team1604-e68a9/us-central1/api
+// http://localhost:5001/team1604-e68a9/us-central1/api  
 
 const {
     helloWorld,
